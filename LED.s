980f337a4017aac14040457d02ad0d78c1d572fc
ecw19 <ecw19@skbl-422-055.ic.ac.uk>
2023-03-10 12:32:46 +0000

    added method

--- LED.s
+++ LED.s
@@ -1,8 +1,9 @@
     #include <xc.inc>
 
-global LED_setup, all_on, all_off
+global LED_setup, all_on, all_off, c2_on, c1_on, r4_on, c2_on_c1_on, c2_off_c1_on, c2_on_c1_off, r4_off, c2_off_c1_off
+
     
-;psect udata_acs	;reserving space in access ram
+;psect udata_acs			; reserving space in access ram
     
 psect LED_code, class = CODE
 
@@ -11,7 +12,7 @@ LED_setup:
     movlw 0x00
     movwf TRISC, A
     movlw 0x00
-    movwf TRISG, A  ;setting PWM pins to outputs
+    movwf TRISG, A			; setting PWM pins to outputs
     return 
     
 all_on: 
@@ -26,4 +27,44 @@ all_off:
     movwf PORTC, A
     movlw 0x00
     movwf PORTG, A
+    return
+    
+c2_on:
+    movlw 00000100B
+    movwf PORTC, A
+    return 
+    
+c1_on:
+    movlw 00000010B
+    movwf PORTC, A
+    return
+   
+c2_on_c1_on:
+    movlw 00000110B
+    movwf PORTC, A
+    return
+
+r4_on:
+    movlw 00010000B
+    movwf PORTG, A
+    return
+    
+c2_off_c1_on:
+    movlw 00000010B
+    movwf PORTC, A
+    return 
+    
+c2_on_c1_off:
+    movlw 00000100B
+    movwf PORTC, A
+    return 
+    
+r4_off:
+    movlw 0x00
+    movwf PORTG, A
+    return
+   
+c2_off_c1_off:
+    movlw 0x00
+    movwf PORTC, A
     return
