25ae254f4e37aff9ba32cd63c5c7f6341bc94d69
ecw19 <ecw19@skbl-422-055.ic.ac.uk>
2023-03-14 12:11:53 +0000

    -more testing

--- main.s
+++ main.s
@@ -1,7 +1,8 @@
 	#include <xc.inc>
 
-extrn   LED_setup, all_on, all_off
-extrn	flashing1, flashing2, flashing3
+extrn   LED_setup, all_on, all_off, g4_on, c2_on_c1_on, c2_off_c1_on, c2_on_c1_off, g4_off, c2_off_c1_off
+extrn	flashing1, flashing2, flashing3, audi, brightness1, brightness2, brightness3, audi_s_line
+extrn	button_setup, button_int
 
 psect	udata_acs			; reserve data space in access ram
 delay_count:ds 1			; reserve one byte for counter in the delay routine
@@ -15,36 +16,18 @@ main:
 	;movlw 	0x00
 	;movwf	TRISE, A		; Port E all outputs
 	call	LED_setup
+	call	button_setup
 	;goto	board_LED
 	goto	flash_test
 	org	0x100			; Main code starts here at address 0x100
 	
-board_LED:				; turns the LEDs on the clicker 2 on 
-	;call	all_on
-	movlw	0x00
-	movwf	PORTD, A
-	movlw	0x00
-	movwf	PORTE, A
-	movlw	0xff
-	movwf	PORTD, A
-	movlw	0xff
-	movwf	PORTE, A
-	goto	board_LED
-
-LED_pins:
-	;movlw	0x0f			; turns on all LEDs on proto-board - control brightness with delay
-	;movwf	delay_count, A 
-	call	all_on
-	;call	all_off
-	;call	delay
-	goto	$
 	
+int_hi:	
+    org	    0x0008			; high vector, no low vecto
+    goto    button_int
 	
 flash_test:
-	call	flashing1
+	call	audi_s_line
     
-delay:	decfsz	delay_count, A		; decrement until zero
-	bra	delay
-	return
 	
 	end	main
