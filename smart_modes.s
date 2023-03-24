f982de9da5b1bae5d28a786a8836c208cbdd0470
ecw19 <ecw19@skbl-422-055.ic.ac.uk>
2023-03-24 09:54:35 +0000

    holy shit PWM worked

--- smart_modes.s
+++ smart_modes.s
@@ -9,10 +9,17 @@ psect smart_code, class = CODE
  
 PWM_setup:
    ; PORT pins are already set to outputs 
-   movlw    00000100
-   movwf    T2CON			   ; configuring timer2
+   movlw    0xff
+   movwf    PR2
    movlw    00001100B			
-   movwf    CCP5CON			   ; enable PWM mode - need to do this for other pins as well (they are ECCP so may be slightly different)
+   movwf    CCP5CON	
+   movlw    0x3F
+   movwf    CCPR5L 
+   movlw    00000100B
+   movwf    T2CON			   ; configuring timer2
+  
+		   ; enable PWM mode - need to do this for other pins as well (they are ECCP so may be slightly different)
+   
    return
    
 disable_PWM:
@@ -22,6 +29,6 @@ disable_PWM:
     
 smart_mode:
      call	sensor_chirp
-     bra	smart_brightness
+     bra	smart_mode
 
 
