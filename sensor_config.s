9fa4f003c414dc972c27e69d8de2f5c54da3fdd4
ecw19 <ecw19@skbl-422-055.ic.ac.uk>
2023-03-29 15:29:08 +0100

    final analysis

--- sensor_config.s
+++ sensor_config.s
@@ -1,14 +1,17 @@
     #include <xc.inc>
 
-global	sensor_chirp, distance
+global	sensor_chirp, distance, interpret_pulse, delay1S, sensor_delay1
     
-extrn	smart_brightness
+extrn	smart_mode, interrupt_state, execute_interrupt
     
 psect udata_acs			; reserving space in access ram
 pulse_delay:	ds 1
 casc_delay:	ds 1
 distance:	ds 1
 interpret_delay:ds 1
+sensor_delay1:  ds 1
+sensor_delay2:  ds 1
+sensor_delay3:  ds 1
  
 psect sensor_code, class = CODE
 
@@ -17,9 +20,13 @@ psect sensor_code, class = CODE
  
 sensor_chirp: ; might change this to "find_distance"
     call    send_pulse
+    movlw   0xff
+    movwf   sensor_delay1
+    call    delay1S
     call    interpret_pulse
     return
-   
+
+
  
 send_pulse:
     movlw   0x00
@@ -39,50 +46,121 @@ send_pulse:
     return
     
 interpret_pulse:
-    ;delay
-    ; BTFSS RH0, A	    ;check if pulse has returned
-    ; call set_distance1
-    ;delay
-    ; BTFSS RH0, A
-    ; call set_distance2
-    ;delay
-    ; BTFSS RH0, A
-    ; call set_distance3
-    ;delay
-    ; BTFSS RH0, A
-    ; call set_distance4
-    ;delay
-    ; BTFSS RH0, A
-    ; call set_distance5
+    call    set_delay_time
+    call    delay2S
+    BTFSS   PORTH, 0, A	    ;check if pulse has returned
+    call    set_distance1
+    call    set_delay_time
+    call    delay2S
+    BTFSS   PORTH, 0, A	
+    call    set_distance2
+    call    set_delay_time
+    call    delay2S
+    BTFSS   PORTH, 0, A	
+    call    set_distance3
+    call    set_delay_time
+    call    delay2S
+    BTFSS   PORTH, 0, A	
+    call    set_distance4
+    call    set_delay_time
+    call    delay2S
+    BTFSS   PORTH, 0, A	
+    call    set_distance5
+    return
+    
+set_delay_time:
+    movlw   0xff
+    movwf   sensor_delay1
+    movlw   0x05
+    movwf   sensor_delay2
     return
     
 set_distance1:
-    ; set PWM duty cycle
+    movlw    0xff			    
+    movwf    CCPR5L
+    movlw    0xff			   
+    movwf    CCPR1L 
+    movlw    0xff			   
+    movwf    CCPR2L 
+    ;goto $
     ; delay		;allows for time between distance readings (has to be at least length of maximum distance)
-    ; check for interrupt
-    ; call smart_mode
+    ;BTFSC   interrupt_state, 0, A
+    ;call    execute_interrupt
+    call    smart_mode
+    return
     
 set_distance2:
-    ; set PWM duty cycle
+    movlw    0xCC			    
+    movwf    CCPR5L
+    movlw    0xCC			   
+    movwf    CCPR1L 
+    movlw    0xCC			   
+    movwf    CCPR2L 
+    ;goto $
     ; delay		;allows for time between distance readings (has to be at least length of maximum distance)
-    ; check for interrupt
-    ; call smart_mode
-set_distance3:
-    ; set PWM duty cycle
-    ; delay		;allows for time between distance readings (has to be at least length of maximum distance)
-    ; check for interrupt
-    ; call smart_mode
-set_distance4:
-    ; set PWM duty cycle
-    ; delay		;allows for time between distance readings (has to be at least length of maximum distance)
-    ; check for interrupt
-    ; call smart_mode
-set_distance5:
-    ; set PWM duty cycle
-    ; delay		;allows for time between distance readings (has to be at least length of maximum distance)
-    ; check for interrupt
-    ; call smart_mode
+    ;BTFSC   interrupt_state, 0, A
+    ;call    execute_interrupt
+    call    smart_mode
+    return
     
+set_distance3:
+    movlw    0x99			    
+    movwf    CCPR5L
+    movlw    0x99			   
+    movwf    CCPR1L 
+    movlw    0x99			   
+    movwf    CCPR2L
+    ;goto $
+    ;delay		;allows for time between distance readings (has to be at least length of maximum distance)
+    ;BTFSC   interrupt_state, 0, A
+    ;call    execute_interrupt
+    call    smart_mode
+    return
+    
+set_distance4:
+    movlw    0x66			    
+    movwf    CCPR5L
+    movlw    0x66			   
+    movwf    CCPR1L 
+    movlw    0x66			   
+    movwf    CCPR2L 
+    ;goto $
+    ; delay		;allows for time between distance readings (has to be at least length of maximum distance)
+    ;BTFSC   interrupt_state, 0, A
+    ;call    execute_interrupt
+    call    smart_mode
+    return
+    
+set_distance5:
+    movlw    0x33			    
+    movwf    CCPR5L
+    movlw    0x33			   
+    movwf    CCPR1L 
+    movlw    0x33			   
+    movwf    CCPR2L 
+    ;goto $
+    ; delay		;allows for time between distance readings (has to be at least length of maximum distance)
+    ;BTFSC   interrupt_state, 0, A
+    ;call    execute_interrupt
+    call    smart_mode
+    return
+    
+delay1S:
+    decfsz  sensor_delay1, A		; decrement until zero
+    bra	    delay1S
+    return
+	
+delay2S:
+    call    delay1S
+    decfsz  sensor_delay2, A
+    bra	    delay2S
+    return
+    
+delay3S:
+    call    delay2S
+    decfsz  sensor_delay3, A
+    bra	    delay3S
+    return
     
 simple_delay:
     decfsz  pulse_delay, A
@@ -94,4 +172,3 @@ casc_delay1:
     decfsz  pulse_delay, A
     bra	    casc_delay1
     return
-
